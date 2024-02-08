a84ac4fd00ac84a43f079063ce4c9010eafc30b9
Don Aldrin Beringuela <101075983+DRin2203@users.noreply.github.com>
2024-02-09 01:16:30 +0800

    adding tables for 2024 week 2 and inserting entries from csv

--- 2024/SQL/table_creations/wk_1.sql
+++ 2024/SQL/table_creations/wk_1.sql
@@ -2,5 +2,32 @@ CREATE SCHEMA year2024
 CREATE TABLE wk_1 ( flight_details VARCHAR(100),
 																				flow_card SMALLINT, bags_checked SMALLINT, meal_type VARCHAR(50));
 
+
+CREATE TABLE year2024.wk_2_flow_card ( 
+	dates Date,
+	flight_number VARCHAR(10),
+	from_location VARCHAR(20),
+	to_location VARCHAR(20),
+	class VARCHAR(20),
+	price DECIMAL,
+	flow_card VARCHAR(5),
+	bags_checked SMALLINT,
+	meal_type VARCHAR(20)
+);
+
+CREATE TABLE year2024.wk_2_non_flow_card ( 
+	dates Date,
+	flight_number VARCHAR(10),
+	from_location VARCHAR(20),
+	to_location VARCHAR(20),
+	class VARCHAR(20),
+	price DECIMAL,
+	flow_card VARCHAR(5),
+	bags_checked SMALLINT,
+	meal_type VARCHAR(20)
+);
+
 -- Inserting csv file to table:
 -- \copy year2024.wk_1(flight_details, flow_card, bags_checked, meal_type) FROM 'C:<file location>\PD 2024 Wk 1 Input.csv' DELIMITER ',' CSV HEADER;
+-- \copy year2024.wk_2_flow_card(dates, flight_number, from_location, to_location, class, price, flow_card, bags_checked, meal_type) FROM 'C:\<file location>\preppin-data-solutions\preppin-data-challenges\2024\Raw Files\PD 2024 Wk 1 Output Flow Card.csv' DELIMITER ',' CSV HEADER;
+-- \copy year2024.wk_2_non_flow_card(dates, flight_number, from_location, to_location, class, price, flow_card, bags_checked, meal_type) FROM 'C:\<file location>\preppin-data-solutions\preppin-data-challenges\2024\Raw Files\PD 2024 Wk 1 Output Non-Flow Card.csv' DELIMITER ',' CSV HEADER;
