fd959fb55019fda0f69d38dad50c7cc21350f916
Don Aldrin Beringuela <101075983+DRin2203@users.noreply.github.com>
2024-02-12 02:38:52 +0800

    Changed queries for sales to align with the required output

--- 2024/SQL/week_3.sql
+++ 2024/SQL/week_3.sql
@@ -29,88 +29,50 @@ FROM year2024.wk_3_q4;
 -- Join the Targets data on to the Sales data (note - you should have 48 rows of data after the join)
 -- Calculate the difference between the Sales and Target values per Class and Month
 -- Output the data
+ -- USING VIEW wk_1_prepped as sales file
 
 CREATE
-TEMPORARY VIEW wk3_q1 AS
+TEMPORARY VIEW sales AS WITH CTE AS
+       (SELECT EXTRACT(MONTH
+                       FROM "Date") as month,
+               LEFT("Class", 1) AS class,
+               SUM("Price") AS price
+        FROM wk_1_prepped
+        GROUP BY 1,
+                 2
+        ORDER BY 1,
+                 2)
 SELECT month,
        CASE
-           WHEN class = 'FC' THEN 'E'
+           WHEN class = 'F' THEN 'E'
            WHEN class = 'E' THEN 'FC'
-           WHEN class = 'BC' THEN 'PE'
+           WHEN class = 'P' THEN 'PE'
            ELSE 'BC'
        END AS class,
-       target
-FROM year2024.wk_3_q1;
-
-
-CREATE
-TEMPORARY VIEW wk3_q2 AS
-SELECT month,
-       CASE
-           WHEN class = 'FC' THEN 'E'
-           WHEN class = 'E' THEN 'FC'
-           WHEN class = 'BC' THEN 'PE'
-           ELSE 'BC'
-       END AS class,
-       target
-FROM year2024.wk_3_q2;
-
-
-CREATE
-TEMPORARY VIEW wk3_q3 AS
-SELECT month,
-       CASE
-           WHEN class = 'FC' THEN 'E'
-           WHEN class = 'E' THEN 'FC'
-           WHEN class = 'BC' THEN 'PE'
-           ELSE 'BC'
-       END AS class,
-       target
-FROM year2024.wk_3_q3;
-
-
-CREATE
-TEMPORARY VIEW wk3_q4 AS
-SELECT month,
-       CASE
-           WHEN class = 'FC' THEN 'E'
-           WHEN class = 'E' THEN 'FC'
-           WHEN class = 'BC' THEN 'PE'
-           ELSE 'BC'
-       END AS class,
-       target
-FROM year2024.wk_3_q4; -- USING VIEW wk_1_prepped as sales file
-
-
-CREATE
-TEMPORARY VIEW sales AS
-SELECT EXTRACT(MONTH
-               FROM "Date") as month,
-       LEFT("Class", 1) AS class,
-       SUM("Price") AS price
-FROM wk_1_prepped
-GROUP BY 1,
-         2;
+       price
+FROM CTE;
 
 
 SELECT *
-FROM sales;
+FROM sales
+ORDER BY month,
+         class;
 
 -- Combining all quarter targets first before merging with the sales data
 
 CREATE
 TEMPORARY VIEW targets AS
 SELECT *
-FROM wk3_q1
+FROM year2024.wk_3_q1
 UNION ALL
 SELECT *
-FROM wk3_q2
+FROM year2024.wk_3_q2
 UNION ALL
 SELECT *
-FROM wk3_q3
+FROM year2024.wk_3_q3
 UNION ALL
 SELECT *
-FROM wk3_q4;
+FROM year2024.wk_3_q4;
 
 
 SELECT *
@@ -121,13 +83,12 @@ FROM targets;
 SELECT (s.price - t.target) AS "Difference to Target",
        s.month AS "Date",
        s.price AS "Price",
-       CASE
-           WHEN t.class = 'FC' THEN 'E'
-           WHEN t.class = 'E' THEN 'FC'
-           WHEN t.class = 'BC' THEN 'PE'
-           ELSE 'BC'
-       END AS "Class",
+       t.class AS "Class",
        t.target AS "Target"
 FROM sales s
-JOIN targets t ON s.month = t.month
-AND s.class = LEFT(t.class, 1);
+JOIN targets t ON (s.month = t.month
+                   AND s.class = t.class);
+
+-- REMARKS:
+-- 1. At first if the values for price for BC and PE are interchanged it will not be the same with the required outputs.
+--     So I retained the values for each except the Economy and First Class Price.
