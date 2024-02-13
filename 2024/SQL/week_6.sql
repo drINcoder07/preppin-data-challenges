0fa7f69381db0be41f10b695f448b297e3107b23
Don Aldrin Beringuela <101075983+DRin2203@users.noreply.github.com>
2024-02-14 00:14:18 +0800

    Changes to Wk 6 sql file

--- 2024/SQL/week_6.sql
+++ 2024/SQL/week_6.sql
@@ -24,23 +24,23 @@ SELECT *
 FROM year2024.wk_6_salaries
 WHERE (staff_id,
        row_number) IN
-    (SELECT staff_id,
-            MAX(row_number)
-     FROM year2024.wk_6_salaries
-     GROUP BY staff_id);
+        (SELECT staff_id,
+                MAX(row_number)
+         FROM year2024.wk_6_salaries
+         GROUP BY staff_id);
 
 -- UNPIVOTING THE data
 
 CREATE VIEW unpivoted_data AS WITH adjusted_data AS
-  (SELECT *
-   FROM year2024.wk_6_salaries
-   WHERE (staff_id,
-          row_number) IN
-       (SELECT staff_id,
-               MAX(row_number) -- Find the latest row (largest row number) to capture the individuals correct salary information
+    (SELECT *
+     FROM year2024.wk_6_salaries
+     WHERE (staff_id,
+            row_number) IN
+             (SELECT staff_id,
+                     MAX(row_number) -- Find the latest row (largest row number) to capture the individuals correct salary information
 
-        FROM year2024.wk_6_salaries
-        GROUP BY staff_id))
+              FROM year2024.wk_6_salaries
+              GROUP BY staff_id))
 SELECT s.staff_id,
        'month_'||t.i AS month,
        t.salary
@@ -54,33 +54,33 @@ FROM unpivoted_data;
 
 -- FINAL OUTPUT!!
 WITH total_salaries AS
-  (SELECT staff_id,
-          SUM(salary) AS total_salary
-   FROM unpivoted_data
-   GROUP BY 1),
+    (SELECT staff_id,
+            SUM(salary) AS total_salary
+     FROM unpivoted_data
+     GROUP BY 1),
      total_salaries_categorized AS
-  (SELECT staff_id,
-          total_salary,
-          CASE
-              WHEN total_salary BETWEEN 12571 AND 50270 THEN '20% rate'
-              WHEN total_salary BETWEEN 50271 AND 125140 THEN '40% rate'
-              ELSE '45% rate'
-          END AS max_tax_rate,
-          CASE
-              WHEN total_salary BETWEEN 12571 AND 50270 THEN 0.2*(total_salary-12570)
-              WHEN total_salary < 12571 THEN 0
-              ELSE (50270 - 12570)*0.2
-          END AS rate_tax_paid_20_pct,
-          CASE
-              WHEN total_salary BETWEEN 50271 AND 125140 THEN 0.4*(total_salary-50270)
-              WHEN total_salary < 50270 THEN 0
-              ELSE (125140 - 50270)*.4
-          END AS rate_tax_paid_40_pct,
-          CASE
-              WHEN total_salary > 125140 THEN 0.45*(total_salary-125140)
-              ELSE 0
-          END AS rate_tax_paid_45_pct
-   FROM total_salaries)
+    (SELECT staff_id,
+            total_salary,
+            CASE
+                WHEN total_salary BETWEEN 12571 AND 50270 THEN '20% rate'
+                WHEN total_salary BETWEEN 50271 AND 125140 THEN '40% rate'
+                ELSE '45% rate'
+            END AS max_tax_rate,
+            CASE
+                WHEN total_salary BETWEEN 12571 AND 50270 THEN 0.2*(total_salary-12570)
+                WHEN total_salary < 12571 THEN 0
+                ELSE (50270 - 12570)*0.2
+            END AS rate_tax_paid_20_pct,
+            CASE
+                WHEN total_salary BETWEEN 50271 AND 125140 THEN 0.4*(total_salary-50270)
+                WHEN total_salary < 50270 THEN 0
+                ELSE (125140 - 50270)*.4
+            END AS rate_tax_paid_40_pct,
+            CASE
+                WHEN total_salary > 125140 THEN 0.45*(total_salary-125140)
+                ELSE 0
+            END AS rate_tax_paid_45_pct
+     FROM total_salaries)
 SELECT staff_id,
        total_salary as salary,
        max_tax_rate,
