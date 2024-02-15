34e78d8d895ad651f7057cf67ec2a24a154dd65d
Don Aldrin Beringuela <101075983+DRin2203@users.noreply.github.com>
2024-02-16 00:22:05 +0800

    Adjusted format for week 1 tables sql

--- 2023/SQL/tables.sql
+++ 2023/SQL/tables.sql
@@ -1,5 +1,5 @@
-CREATE TABLE year2023.wk_1 ( transaction_code VARCHAR(30),
-                                              value INTEGER, customer_code VARCHAR(25),
-                                                                           online_or_in_person SMALLINT, transaction_date DATE);
+CREATE TABLE year2023.wk_1 (transaction_code VARCHAR(30),
+                                             value INTEGER, customer_code VARCHAR(25),
+                                                                          online_or_in_person SMALLINT, transaction_date DATE);
 
 -- \copy year2023.wk_1(transaction_code, value, customer_code, online_or_in_person, transaction_date) FROM 'C:\Users\drin\Desktop\PROJECTS\preppin-data-solutions\preppin-data-challenges\2023\Raw Files\PD 2023 Wk 1 Input.csv' DELIMITER ',' CSV HEADER;
