6e4c49538e2d7651c5f88c29bcb4ab16d4a70fc2
Don Aldrin Beringuela <101075983+DRin2203@users.noreply.github.com>
2024-02-24 15:52:11 +0800

    Added table and insert data for Week 3 2023

--- 2023/SQL/tables.sql
+++ 2023/SQL/tables.sql
@@ -4,13 +4,18 @@ CREATE TABLE year2023.wk_1 (transaction_code VARCHAR(30),
 
 -- \copy year2023.wk_1(transaction_code, value, customer_code, online_or_in_person, transaction_date) FROM 'C:\Users\drin\Desktop\PROJECTS\preppin-data-solutions\preppin-data-challenges\2023\Raw Files\PD 2023 Wk 1 Input.csv' DELIMITER ',' CSV HEADER;
 
-CREATE TABLE year2023.wk_2_transactions ( transaction_id INTEGER, account_number INTEGER, sort_code VARCHAR(15),
-                                                                                                    bank VARCHAR(30));
+CREATE TABLE year2023.wk_2_transactions (transaction_id INTEGER, account_number INTEGER, sort_code VARCHAR(15),
+                                                                                                   bank VARCHAR(30));
 
 
-CREATE TABLE year2023.wk_2_swift_codes ( bank VARCHAR(30),
-                                              swift_code VARCHAR(4),
-                                                         check_digits VARCHAR(2));
+CREATE TABLE year2023.wk_2_swift_codes (bank VARCHAR(30),
+                                             swift_code VARCHAR(4),
+                                                        check_digits VARCHAR(2));
 
 -- \copy year2023.wk_2_transactions(transaction_id, account_number, sort_code, bank ) FROM 'C:\Users\drin\Desktop\PROJECTS\preppin-data-solutions\preppin-data-challenges\2023\Raw Files\Transactions.csv' DELIMITER ',' CSV HEADER;
 -- \copy year2023.wk_2_swift_codes(bank, swift_code, check_digits) FROM 'C:\Users\drin\Desktop\PROJECTS\preppin-data-solutions\preppin-data-challenges\2023\Raw Files\Swift Codes.csv' DELIMITER ',' CSV HEADER;
+
+CREATE TABLE year2023.wk_3_targets (online_or_in_person VARCHAR(20),
+                                                        q1 INTEGER, q2 INTEGER, q3 INTEGER, q4 INTEGER);
+
+-- \copy year2023.wk_3_targets(online_or_in_person, q1, q2, q3, q4) FROM 'C:\Users\drin\Desktop\PROJECTS\preppin-data-solutions\preppin-data-challenges\2023\Raw Files\Targets.csv' DELIMITER ',' CSV HEADER;
