95f50daa55db52208a270f2e2e7d70c876e0828b
Don Aldrin Beringuela <101075983+DRin2203@users.noreply.github.com>
2024-02-24 15:09:33 +0800

    Removed unnecessary phrases

--- 2023/SQL/week_2.sql
+++ 2023/SQL/week_2.sql
@@ -6,13 +6,13 @@ SELECT *
 FROM year2023.wk_2_transactions;
 
 --TASKS
--- In the Transactions table, there is a Sort Code field which contains dashes. We need to remove these so just have a 6 digit string (hint)
--- Use the SWIFT Bank Code lookup table to bring in additional information about the SWIFT code and Check Digits of the receiving bank account (hint)
--- Add a field for the Country Code (hint)
+-- In the Transactions table, there is a Sort Code field which contains dashes. We need to remove these so just have a 6 digit string
+-- Use the SWIFT Bank Code lookup table to bring in additional information about the SWIFT code and Check Digits of the receiving bank account
+-- Add a field for the Country Code
 -- Hint: all these transactions take place in the UK so the Country Code should be GB
--- Create the IBAN as above (hint)
+-- Create the IBAN as above
 -- Hint: watch out for trying to combine sting fields with numeric fields - check data types
--- Remove unnecessary fields (hint)
+-- Remove unnecessary fields
 -- Output the data
 
 SELECT t.transaction_id,
