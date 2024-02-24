2accfc93955deed010e4dc01e3d70ecc477c3ea7
Don Aldrin Beringuela <101075983+DRin2203@users.noreply.github.com>
2024-02-24 14:50:03 +0800

    Creating tables and inserting data from csv files to week 2 2023

--- 2023/SQL/tables.sql
+++ 2023/SQL/tables.sql
@@ -3,3 +3,14 @@ CREATE TABLE year2023.wk_1 (transaction_code VARCHAR(30),
                                                                           online_or_in_person SMALLINT, transaction_date DATE);
 
 -- \copy year2023.wk_1(transaction_code, value, customer_code, online_or_in_person, transaction_date) FROM 'C:\Users\drin\Desktop\PROJECTS\preppin-data-solutions\preppin-data-challenges\2023\Raw Files\PD 2023 Wk 1 Input.csv' DELIMITER ',' CSV HEADER;
+
+CREATE TABLE year2023.wk_2_transactions ( transaction_id INTEGER, account_number INTEGER, sort_code VARCHAR(15),
+                                                                                                    bank VARCHAR(30));
+
+
+CREATE TABLE year2023.wk_2_swift_codes ( bank VARCHAR(30),
+                                              swift_code VARCHAR(4),
+                                                         check_digits VARCHAR(2));
+
+-- \copy year2023.wk_2_transactions(transaction_id, account_number, sort_code, bank ) FROM 'C:\Users\drin\Desktop\PROJECTS\preppin-data-solutions\preppin-data-challenges\2023\Raw Files\Transactions.csv' DELIMITER ',' CSV HEADER;
+-- \copy year2023.wk_2_swift_codes(bank, swift_code, check_digits) FROM 'C:\Users\drin\Desktop\PROJECTS\preppin-data-solutions\preppin-data-challenges\2023\Raw Files\Swift Codes.csv' DELIMITER ',' CSV HEADER;
